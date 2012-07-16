80cc7d5d58ba0493dbbd2a515cc1f319291c810b
Ievgen Voloshchuk <evoloshchuk@gmail.com>
2012-07-16 22:03:05 +0200

    switch to xhr-polling as a transport

--- websockets.js
+++ websockets.js
@@ -7,6 +7,13 @@ exports.attach = function(server) {
     // Start listening.
     var io = sio.listen(server);
 
+    // It is a bit dissapointing, but Heroku Cedar stack does not support
+    // websockets at the moment, so we have to use xhr-polling.
+    io.configure(function () { 
+        io.set("transports", ["xhr-polling"]); 
+        io.set("polling duration", 300);
+    });
+
     /**
      * For every new incoming connection let's return the id of this connection
      * back to the client, so it can be passed around, so it can be found later
